147bce889476c1dcdac03288541acf12f6a40119
Gwatamalou <96253655+Gwatamalou@users.noreply.github.com>
2024-07-30 19:58:18 +0600

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,1 @@
-import requests
-from pprint import pprint
 
-q = {'q': 'Moscow', 'appid': '1b417740c98e2c1ca400c10eb4312013', 'units': 'metric'}
-URL = f'http://api.openweathermap.org/data/2.5/weather'
-
-r = requests.get(URL, params=q)
-
-pprint(r)
